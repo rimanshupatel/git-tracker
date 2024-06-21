695b551f037751eba1af38717558474b52f47ad7
rimanshupatel <rimanshupatel1@gmail.com>
2024-06-21 09:30:43 +0530

    adding react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className=" flex flex-col justify-center items-center w-[741.99px] m-auto">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* 👈 Renders at /app/ */}
+        </Routes>
       </div>
     </>
   );
